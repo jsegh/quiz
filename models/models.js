9ba86289db449d211dc1a33ed18cc52ef1e55a6b
javsanz <jsegh00@gmail.com>
2015-08-02 00:55:48 +0200

    correccion entrega 8

--- models/models.js
+++ models/models.js
@@ -45,7 +45,7 @@ exports.Quiz = Quiz;//exporta definicion de tabla quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 //sequelize.sync().then(function() {
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
   // success(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().success(function (count){
 	  if(count === 0) {   // la tabla se inicializa solo si está vacía
